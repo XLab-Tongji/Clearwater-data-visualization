a9a881a865110f9110ebbb9494d522055d0d6a6c
leonnnop <leonliang@liangchendeMacBook-Pro.local>
2018-10-30 14:52:53 +0800

    frontend&backend update

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -5,7 +5,8 @@
         <el-header height="20%">
           <el-row>
             <el-col :span="10">
-              <el-input placeholder="Please keywords to search for specific resource" prefix-icon="el-icon-search" v-model="keyword">
+              <el-input placeholder="Please keywords to search for specific resource" prefix-icon="el-icon-search"
+                v-model="keyword">
               </el-input>
             </el-col>
             <el-col :span="4">
@@ -19,7 +20,8 @@
               </el-radio-group>
             </el-col>
             <el-col :offset="2" :span="9">
-              <label style="color:white; display: block;"><input style="width:240px; margin-top: 20pt;" type="range" min="0" max="0.2" step="any" value="0.1"> Link Strength</label>
+              <label style="color:white; display: block;"><input style="width:240px; margin-top: 20pt;" type="range"
+                  min="0" max="0.2" step="any" value="0.1"> Link Strength</label>
             </el-col>
           </el-row>
         </el-header>
@@ -32,7 +34,11 @@
           </el-row>
           <el-row>
             <img id="loading" src="./assets/loading.gif" />
-            <svg id="graph" width="1000" height="500"></svg>
+            <!-- <svg id="graph" width="1000" height="500"></svg> -->
+
+            <div id="neo4jd3" width="1000" height="500" style="background:white"></div>
+
+
           </el-row>
           <el-row>
             <img src="./assets/Rectangle.png" />
@@ -46,7 +52,7 @@
               <el-col :offset="2" :span="20">
                 <el-form label-position="top" style="text-align:left;height:50%">
                   <el-form-item label="Id">
-                      <el-input id="this_id" disabled></el-input>
+                    <el-input id="this_id" disabled></el-input>
                   </el-form-item>
                   <el-form-item label="Name">
                     <el-input id="this_name" disabled></el-input>
@@ -97,7 +103,8 @@
                   </el-form-item>
                   <el-form-item label="Failure Type">
                     <el-select v-model="newFailure.type" placeholder="Choose failure type" size="medium" style="display:inline">
-                      <el-option v-for="item in ['Fail slow', 'Fail partial', 'Fail stop', 'Fail transient', 'Fail as well as corruption']" :key="item" :value="item">
+                      <el-option v-for="item in ['Fail slow', 'Fail partial', 'Fail stop', 'Fail transient', 'Fail as well as corruption']"
+                        :key="item" :value="item">
                       </el-option>
                     </el-select>
                   </el-form-item>
@@ -155,16 +162,23 @@
 </template>
 
 <script>
-  import {getAll, addOne, getLabel} from './assets/API'
-  import {showData2} from "./assets/dataV";
+  import {
+    getAll,
+    addOne,
+    getLabel
+  } from './assets/API'
+  import {
+    showData2
+  } from "./assets/dataV";
   import $ from 'jquery'
+  import Neo4JD3 from 'neo4jd3'
 
 
   export default {
     data() {
       return {
-        nodes:[],
-        links:[],
+        nodes: [],
+        links: [],
         activeName: 'Information',
         keyword: '',
         newResource: {},
@@ -175,137 +189,151 @@
       }
     },
     methods: {
-        showAll: function () {
-            var self = this;
-            $.ajax({
-                type: 'get',
-                async: true,
-                crossDomain: true,
-                // url: 'http://localhost:8088/bbs/api/getAll',
-                url: '/bbs/api/getAll',
-                success: function (res) {
-                    self.nodes = res.nodes;
-                    self.links = res.links;
-                    showData2(self.nodes, self.links);
-                    document.getElementById('loading').style.display = 'none';
-                },
-                error: function (res) {
-                    console.log('Something went wrong!')
-                }
-            })
-        },
-        display: function () {
-            document.getElementById('loading').style.display = 'inline';
-            if (this.whichResourceToShow === 'All')
-                this.showAll();
-            else {
-                var labelName;
-                switch (this.whichResourceToShow) {
-                    case 'Resource':
-                        labelName = 'ResourceNode';
-                        break;
-                    case 'Failure':
-                        labelName = 'Failure_Node';
-                        break;
-                    case 'ADR':
-                        labelName = 'ADR_Node';
-                        break;
-                    case 'Test Case':
-                        labelName = 'TestCase_Node';
-                        break;
-                }
-                var self = this;
-                $.ajax({
-                    type: 'post',
-                    async: true,
-                    crossDomain: true,
-                    // url: 'http://localhost:8088/bbs/api/getLabel',
-                    url: '/bbs/api/getLabel',
-                    beforeSend: function (xhr) {
-                        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-                    },
-                    data: {label:labelName},
-                    success: function (res) {
-                        self.nodes = res.nodes;
-                        self.links = res.links;
-                        showData2(self.nodes, self.links);
-                        document.getElementById('loading').style.display = 'none';
-                    },
-                    error: function (res) {
-                        console.log('A Something wrong!')
-                    }
-                })
+      showAll: function () {
+        var self = this;
+        $.ajax({
+          type: 'get',
+          async: true,
+          crossDomain: true,
+          // url: 'http://localhost:8088/bbs/api/getAll',
+          url: '/bbs/api/getAll',
+          success: function (res) {
+            console.log('showall success')
+            self.nodes = res.nodes;
+            self.links = res.links;
+            showData2(self.nodes, self.links);
+            document.getElementById('loading').style.display = 'none';
+          },
+          error: function (res) {
+            console.log('Something went wrong!')
+          }
+        })
+      },
+      display: function () {
+        document.getElementById('loading').style.display = 'inline';
+        if (this.whichResourceToShow === 'All')
+          this.showAll();
+        else {
+          var labelName;
+          switch (this.whichResourceToShow) {
+            case 'Resource':
+              labelName = 'ResourceNode';
+              break;
+            case 'Failure':
+              labelName = 'Failure_Node';
+              break;
+            case 'ADR':
+              labelName = 'ADR_Node';
+              break;
+            case 'Test Case':
+              labelName = 'TestCase_Node';
+              break;
+          }
+          var self = this;
+          $.ajax({
+            type: 'post',
+            async: true,
+            crossDomain: true,
+            // url: 'http://localhost:8088/bbs/api/getLabel',
+            url: '/bbs/api/getLabel',
+            beforeSend: function (xhr) {
+              xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+            },
+            data: {
+              label: labelName
+            },
+            success: function (res) {
+              self.nodes = res.nodes;
+              self.links = res.links;
+              showData2(self.nodes, self.links);
+              document.getElementById('loading').style.display = 'none';
+            },
+            error: function (res) {
+              console.log('A Something wrong!')
             }
-        },
-        add: function (type) {
-            var inputData;
-            switch (type) {
-                case 1:
-                    this.newResource.label = "ResourceNode";
-                    var array = this.newResource.relations.split(", ");
-                    for(var i=0; i<array.length; i++)
-                        array[i] = parseInt(array[i]);
-                    this.newResource.relations = array;
-                    inputData = this.newResource;
-                    break;
-                case 2:
-                    this.newFailure.label = "Failure_Node";
-                    this.newFailure.layer = "0";
-                    var array = this.newFailure.relations.split(", ");
-                    for(var i=0; i<array.length; i++)
-                        array[i] = parseInt(array[i]);
-                    this.newFailure.relations = array;
-                    inputData = this.newFailure;
-                    break;
-                case 3:
-                    this.newADR.label = "ADR_Node";
-                    this.newADR.layer = "0";
-                    this.newADR.type = "ADR;"
-                    var array = this.newADR.relations.split(", ");
-                    for(var i=0; i<array.length; i++)
-                        array[i] = parseInt(array[i]);
-                    this.newADR.relations = array;
-                    inputData = this.newADR;
-                    break;
-                case 4:
-                    this.newTestCase.label = "TestCase_Node";
-                    this.newTestCase.layer = "0";
-                    this.newTestCase.type = "Test Case;"
-                    var array = this.newTestCase.relations.split(", ");
-                    for(var i=0; i<array.length; i++)
-                        array[i] = parseInt(array[i]);
-                    this.newTestCase.relations = array;
-                    inputData = this.newTestCase;
-                    break;
-            }
-            console.log(inputData);
-            var result = addOne(inputData);
-            alert(result.state);
-            this.display();
-        },
-        search: function () {
-            var self = this;
-            document.getElementById('loading').style.display = 'inline';
-            $.ajax({
-                type: 'post',
-                async: true,
-                crossDomain: true,
-                url: 'http://localhost:8088/bbs/api/getNeighbors',
-                beforeSend: function (xhr) {
-                    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-                },
-                data: {name:self.keyword},
-                success: function (res) {
-                    self.links = res.links
-                    self.nodes = res.nodes
-                    showData2(self.nodes, self.links)
-                    document.getElementById('loading').style.display = 'none'
-                },
-                error: function (res) {
-                    console.log('A Something wrong!')
-                }
-            })
+          })
         }
+      },
+      add: function (type) {
+        var inputData;
+        switch (type) {
+          case 1:
+            this.newResource.label = "ResourceNode";
+            var array = this.newResource.relations.split(", ");
+            for (var i = 0; i < array.length; i++)
+              array[i] = parseInt(array[i]);
+            this.newResource.relations = array;
+            inputData = this.newResource;
+            break;
+          case 2:
+            this.newFailure.label = "Failure_Node";
+            this.newFailure.layer = "0";
+            var array = this.newFailure.relations.split(", ");
+            for (var i = 0; i < array.length; i++)
+              array[i] = parseInt(array[i]);
+            this.newFailure.relations = array;
+            inputData = this.newFailure;
+            break;
+          case 3:
+            this.newADR.label = "ADR_Node";
+            this.newADR.layer = "0";
+            this.newADR.type = "ADR;"
+            var array = this.newADR.relations.split(", ");
+            for (var i = 0; i < array.length; i++)
+              array[i] = parseInt(array[i]);
+            this.newADR.relations = array;
+            inputData = this.newADR;
+            break;
+          case 4:
+            this.newTestCase.label = "TestCase_Node";
+            this.newTestCase.layer = "0";
+            this.newTestCase.type = "Test Case;"
+            var array = this.newTestCase.relations.split(", ");
+            for (var i = 0; i < array.length; i++)
+              array[i] = parseInt(array[i]);
+            this.newTestCase.relations = array;
+            inputData = this.newTestCase;
+            break;
+        }
+        console.log(inputData);
+        var result = addOne(inputData);
+        alert(result.state);
+        this.display();
+      },
+      search: function () {
+        var self = this;
+        document.getElementById('loading').style.display = 'inline';
+        $.ajax({
+          type: 'post',
+          async: true,
+          crossDomain: true,
+          url: 'http://localhost:8088/bbs/api/getNeighbors',
+          beforeSend: function (xhr) {
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+          },
+          data: {
+            name: self.keyword
+          },
+          success: function (res) {
+            self.links = res.links
+            self.nodes = res.nodes
+            showData2(self.nodes, self.links)
+            document.getElementById('loading').style.display = 'none'
+          },
+          error: function (res) {
+            console.log('A Something wrong!')
+          }
+        })
+      }
+    },
+    // method
+    created() {
+      new Neo4JD3('#neo4jd3', {
+        // neo4jData: data,
+        neo4jDataUrl: './assets/neo4jData.json',
+        minCollision: 75,
+        nodeRadius: 20
+      })
     }
   }
 </script>
@@ -315,23 +343,27 @@
     font-family: Helvetica, sans-serif;
     text-align: center;
   }
+
   .whole {
     position: absolute;
     width: 100%;
     height: 100%;
   }
+
   .el-header {
     background-color: #232931;
     color: #616161;
     text-align: center;
     line-height: 60px;
   }
+
   .el-aside {
     background-color: #393e46;
     color: #616161;
     text-align: center;
     line-height: 200px;
   }
+
   .el-main {
     background-color: #eeeeee;
     color: #616161;
@@ -339,15 +371,18 @@
     /*line-height: 160px;*/
     position: relative;
   }
+
   .el-icon-arrow-right:before {
     content: "\E62B";
   }
-  #graph{
+
+  #graph {
     border: 1px solid blue;
     max-width: 100%;
     scroll-behavior: auto;
   }
-  #loading{
+
+  #loading {
     position: absolute;
     left: 35%;
     top: 100px;
--- Frontend/src/assets/dataV.js
+++ Frontend/src/assets/dataV.js
@@ -1,14 +1,19 @@
-function showData2 (nodes, links) {
+var d3 = require("d3");
+import $ from 'jquery'
+
+
+function showData2(nodes, links) {
+
     var colorMap = new Map();
-    colorMap.set("\"System\"",'aqua');
-    colorMap.set("\"Service\"",'lightskyblue');
-    colorMap.set("\"Component\"",'deepskyblue');
-    colorMap.set("\"Container\"",'royalblue');
-    colorMap.set("\"Container Resource\"",'blue');
-    colorMap.set("\"VM\"",'navy');
-    colorMap.set("\"Fail-stop\"",'red');
-    colorMap.set("\"ADR\"",'lime');
-    colorMap.set("\"Test Case\"",'gold');
+    colorMap.set("\"System\"", 'aqua');
+    colorMap.set("\"Service\"", 'lightskyblue');
+    colorMap.set("\"Component\"", 'deepskyblue');
+    colorMap.set("\"Container\"", 'royalblue');
+    colorMap.set("\"Container Resource\"", 'blue');
+    colorMap.set("\"VM\"", 'navy');
+    colorMap.set("\"Fail-stop\"", 'red');
+    colorMap.set("\"ADR\"", 'lime');
+    colorMap.set("\"Test Case\"", 'gold');
     var N = JSON.stringify(nodes)
     var E = JSON.stringify(links)
     localStorage.setItem('nodes', N)
@@ -23,19 +28,21 @@ function showData2 (nodes, links) {
     console.log(d3.forceManyBody())
     // 通过布局来转换数据，然后进行绘制
     var simulation = d3.forceSimulation(nodes)
-        .force('link', d3.forceLink(links).id(function (d) { return d.id }))
-        .force('charge', d3.forceManyBody())// 创建多体力
+        .force('link', d3.forceLink(links).id(function (d) {
+            return d.id
+        }))
+        .force('charge', d3.forceManyBody()) // 创建多体力
         .force('center', d3.forceCenter(width / 2, height / 2))
 
     d3.select('input[type = range]')
         .on('input', inputted)
 
     simulation
-        .nodes(nodes)// 设置力模拟的节点
+        .nodes(nodes) // 设置力模拟的节点
         .on('tick', ticked)
 
-    simulation.force('link').strength(0.06)// 添加或移除力
-        .links(links)// 设置连接数组
+    simulation.force('link').strength(0.06) // 添加或移除力
+        .links(links) // 设置连接数组
     var color = d3.scaleOrdinal(d3.schemeCategory20)
     // 绘制
     var svg_links = svg.selectAll('line')
@@ -44,18 +51,24 @@ function showData2 (nodes, links) {
         .append('line')
         .style('stroke', '#ccc')
         .style('stroke-width', 1)
-        .call(d3.zoom()// 创建缩放行为
-            .scaleExtent([-5, 2])// 设置缩放范围
+        .call(d3.zoom() // 创建缩放行为
+            .scaleExtent([-5, 2]) // 设置缩放范围
         )
 
     var svg_nodes = svg.selectAll('circle')
         .data(nodes)
         .enter()
         .append('circle')
-        .attr('cx', function (d) { return d.x })
-        .attr('cy', function (d) { return d.y })
+        .attr('cx', function (d) {
+            return d.x
+        })
+        .attr('cy', function (d) {
+            return d.y
+        })
         .attr('r', '16')
-        .attr('fill', function (d) { return colorMap.get(d.type) })
+        .attr('fill', function (d) {
+            return colorMap.get(d.type)
+        })
         .attr('stroke', 'gray')
         .style('stroke-width', 2)
         .on('dblclick', click)
@@ -77,76 +90,92 @@ function showData2 (nodes, links) {
         .attr('text-anchor', 'middle')
         .attr('font-size', 8)
         .text(function (d) {
-            return d.name.substring(0,8)
+            return d.name.substring(0, 8)
         })
 
-    function dragstarted (d) {
-        if (!d3.event.active) simulation.alphaTarget(0.3).restart()// 设置目标α
+    function dragstarted(d) {
+        if (!d3.event.active) simulation.alphaTarget(0.3).restart() // 设置目标α
         d.fx = d.x
         d.fy = d.y
     }
 
-    function dragged (d) {
+    function dragged(d) {
         d.fx = d3.event.x
         d.fy = d3.event.y
     }
 
-    function dragended (d) {
+    function dragended(d) {
         if (!d3.event.active) simulation.alphaTarget(0)
         d.fx = null
         d.fy = null
     }
 
-    function inputted () {
+    function inputted() {
         simulation.force('link').strength(+this.value)
         simulation.alpha(1).restart();
     }
 
-    function ticked () {
-        svg_links.attr('x1', function (d) { return d.source.x })
-            .attr('y1', function (d) { return d.source.y })
-            .attr('x2', function (d) { return d.target.x })
-            .attr('y2', function (d) { return d.target.y })
+    function ticked() {
+        svg_links.attr('x1', function (d) {
+                return d.source.x
+            })
+            .attr('y1', function (d) {
+                return d.source.y
+            })
+            .attr('x2', function (d) {
+                return d.target.x
+            })
+            .attr('y2', function (d) {
+                return d.target.y
+            })
 
-        svg_nodes.attr('cx', function (d) { return d.x })
-            .attr('cy', function (d) { return d.y })
+        svg_nodes.attr('cx', function (d) {
+                return d.x
+            })
+            .attr('cy', function (d) {
+                return d.y
+            })
 
-        svg_texts.attr('x', function (d) { return d.x })
-            .attr('y', function (d) { return d.y })
+        svg_texts.attr('x', function (d) {
+                return d.x
+            })
+            .attr('y', function (d) {
+                return d.y
+            })
 
     }
 }
 
-function click (d) {
+function click(d) {
     updateData(d.name)
 }
 
 function showDetail(d) {
-    document.getElementById('this_id').value = "id: "+d.id;
-    document.getElementById('this_name').value = "name: "+d.name;
-    document.getElementById('this_type').value = "type: "+d.type;
-    document.getElementById('this_layer').value = "layer: "+d.layer;
-    document.getElementById('this_performance').value = "performance: "+d.performance;
+    document.getElementById('this_id').value = "id: " + d.id;
+    document.getElementById('this_name').value = "name: " + d.name;
+    document.getElementById('this_type').value = "type: " + d.type;
+    document.getElementById('this_layer').value = "layer: " + d.layer;
+    document.getElementById('this_performance').value = "performance: " + d.performance;
 }
 
 function onHover(d) {
-    d3.select(this).style('stroke-width',6)
-    d3.select(this).style('r',18)
-    document.getElementById('id').innerHTML = "id: "+d.id;
-    document.getElementById('name').innerHTML = "name: "+d.name;
-    document.getElementById('type').innerHTML = "type: "+d.type;
-    document.getElementById('layer').innerHTML = "layer: "+d.layer;
+    d3.select(this).style('stroke-width', 6)
+    d3.select(this).style('r', 18)
+    document.getElementById('id').innerHTML = "id: " + d.id;
+    document.getElementById('name').innerHTML = "name: " + d.name;
+    document.getElementById('type').innerHTML = "type: " + d.type;
+    document.getElementById('layer').innerHTML = "layer: " + d.layer;
 }
 
 function moveOut(d) {
-    d3.select(this).style('stroke-width',2)
-    d3.select(this).style('r',16)
+    d3.select(this).style('stroke-width', 2)
+    d3.select(this).style('r', 16)
 }
 
-function updateData (name) {
+function updateData(name) {
     document.getElementById('loading').style.display = 'inline'
     var temp
-    name = name.replace("\"","").replace("\"","")
+    name = name.replace("\"", "").replace("\"", "")
     $.ajax({
         type: 'post',
         async: true,
@@ -155,7 +184,9 @@ function updateData (name) {
         beforeSend: function (xhr) {
             xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
         },
-        data: {name:name},
+        data: {
+            name: name
+        },
         success: function (res) {
             temp = res
             var newLinks = temp.links
@@ -175,7 +206,7 @@ function updateData (name) {
     })
 }
 
-function removeRepeat (x, y, type) {
+function removeRepeat(x, y, type) {
     if (type === 'n') {
         for (var i = 0; i < x.length; i++) {
             for (var j = 0; j < y.length; j++) {
@@ -199,4 +230,7 @@ function removeRepeat (x, y, type) {
     return array
 }
 
-export { showData2, removeRepeat }
+export {
+    showData2,
+    removeRepeat
+}
