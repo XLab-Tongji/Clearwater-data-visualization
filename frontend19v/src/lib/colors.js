ca7faf72d8f567189ecf540af1ba1c7f2e3f9040
sushaoci <you@example.com>
2019-07-19 11:51:07 +0800

    state

--- frontend19v/src/lib/colors.js
+++
@@ -1,8 +0,0 @@
-const colors = ["blueviolet", "coral", "lightgreen", "hotpink"]
-let nodeIdsSet = new Set()
-
-export function getColor(id) {
-  nodeIdsSet.add(id);
-  let index = Array.from(nodeIdsSet).indexOf(id);
-  return colors[index % colors.length];
-}
