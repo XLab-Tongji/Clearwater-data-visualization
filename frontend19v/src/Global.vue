20f0977ba02a8818c8e146e15ea97b562c5c86ed
shuangliz <358355029@qq.com>
2019-08-05 14:40:17 +0800

    update backurl in frontend

--- frontend19v/src/Global.vue
+++ frontend19v/src/Global.vue
@@ -1,6 +1,6 @@
 <script>
-    const backurl='http://127.0.0.1:8088';
-    //const backurl='http://backend:9990';
+    // const backurl='http://127.0.0.1:8088';
+    const backurl='http://10.60.38.173:9990';
     export default
     {
         backurl
