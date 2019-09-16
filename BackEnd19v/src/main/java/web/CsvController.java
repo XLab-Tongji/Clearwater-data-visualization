ab09e1aa7d6907fd80f161cc31ff5da77195af35
ChaunceyJ <33971563+ChaunceyJ@users.noreply.github.com>
2019-09-16 21:48:39 +0800

    code refactoring

---
+++ BackEnd19v/src/main/java/web/CsvController.java
@@ -0,0 +1,108 @@
+package web;
+
+import neo4j.MongoDriver;
+import neo4j.Neo4jDriver;
+import org.apache.commons.fileupload.disk.DiskFileItem;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.yaml.snakeyaml.Yaml;
+import util.TimerUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.*;
+
+import static neo4j.FusekiDriver.*;
+import static neo4j.Neo4jDriver.*;
+import static neo4j.prometheusDriver.DealPrometheusRequest;
+import static neo4j.prometheusDriver.newPrometheus;
+<<<<<<< Updated upstream
+=======
+import static service.CsvService.*;
+>>>>>>> Stashed changes
+
+
+public class CsvController {
+    @RequestMapping(value = "/api/getCSV",method = RequestMethod.GET,produces = "application/json")
+    //获取csv名称
+    public HashMap<String,ArrayList> getCsv(HttpServletRequest request,
+                                            HttpServletResponse response){
+        HashMap<String,ArrayList> hashMap = new HashMap<>();
+        ArrayList list = new ArrayList();
+        String savePath = request.getSession().getServletContext().getRealPath("/WEB-INF/upload/csv");
+        savePath = savePath.replace("file:", "");
+        File file = new File(savePath);
+        if (file.exists()) {
+            File[] files = file.listFiles();
+            try {
+                for (File file2 : files) {
+                    if (file2.getName().contains(".csv"))
+                        list.add(file2.getName());
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        hashMap.put("CSV",list);
+        return hashMap;
+    }
+    @RequestMapping(value = "/api/getOperationData" , method = RequestMethod.GET ,produces = "application/json")
+    //处理运维数据 目前文件类型：csv
+    public HashMap<String,HashMap<String,ArrayList>> getOperationData(HttpServletRequest request,
+                                                                      HttpServletResponse response,@RequestParam("filename") String filename) {
+        String savePath = request.getSession().getServletContext().getRealPath("/WEB-INF/upload/csv");
+        savePath = savePath.replace("file:", "");
+        String filePath = savePath + "/"  + filename;
+        HashMap<String,ArrayList> hashMap = csvOperationData(filePath);
+        HashMap<String,HashMap<String,ArrayList>> map = new HashMap<>();
+        map.put("Operations",hashMap);
+        return map;
+    }
+    @RequestMapping(value = "/api/metricInfo",method = RequestMethod.GET,produces = "application/json")
+    //获取csv属性
+    public HashMap<String,HashMap<String,Object>> getMetricInfo(HttpServletRequest request,
+                                                                HttpServletResponse response,@RequestParam("filename") String filename){
+        String savePath = request.getSession().getServletContext().getRealPath("/WEB-INF/upload/csv");
+        savePath = savePath.replace("file:", "");
+        String filePath = savePath + "/"  + filename;
+        return metricInfo(filePath);
+    }
+    //service/carts:80/response_time contianer/docker_compose_carts/cpu_util
+    @RequestMapping(value = "/api/getTimestamp" , method = RequestMethod.GET ,produces = "application/json")
+    //获取时间戳 目前文件类型：csv
+    public HashMap<String,ArrayList> getTimestamp(HttpServletRequest request,
+                                                  HttpServletResponse response,@RequestParam("filename") String filename) {
+        String savePath = request.getSession().getServletContext().getRealPath("/WEB-INF/upload/csv");
+        savePath = savePath.replace("file:", "");
+        String filePath = savePath + "/"  + filename;
+        ArrayList arrayList = csvTimestamp(filePath);
+        HashMap<String,ArrayList> map = new HashMap<>();
+        map.put("Timestamp",arrayList);
+        return map;
+    }
+    @RequestMapping(value = "/api/postCSV",method = RequestMethod.POST,produces = "application/json")
+    //获取上传csv 获取相应的json和txt文件
+    public Map<String, Object> postCSV(HttpServletRequest request, HttpServletResponse response,
+                                       @RequestParam("algorithm")String algorithm,@RequestParam("filename") String filename){
+        String savePath = request.getSession().getServletContext().getRealPath("/WEB-INF/upload/csv");
+        savePath = savePath.replace("file:", "");
+        String filePath = savePath + "/"  + filename;
+        try{
+            // File file = new File(filePath);
+            System.out.println("66");
+            return csvPost(algorithm,filePath);
+        }catch (Exception e) {
+            System.out.println("66");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
---
+++ BackEnd19v/src/main/java/web/MongoDBController.java
@@ -0,0 +1,113 @@
+package web;
+
+<<<<<<< Updated upstream
+import neo4j.MongoDriver;
+import neo4j.Neo4jDriver;
+import org.apache.commons.fileupload.disk.DiskFileItem;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.yaml.snakeyaml.Yaml;
+import service.MongoService;
+import util.TimerUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.*;
+
+import static neo4j.FusekiDriver.*;
+import static neo4j.Neo4jDriver.*;
+import static neo4j.prometheusDriver.DealPrometheusRequest;
+import static neo4j.prometheusDriver.newPrometheus;
+=======
+
+import org.springframework.web.bind.annotation.*;
+import service.MongoService;
+import util.TimerUtil;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+
+import static neo4j.FusekiDriver.*;
+import static neo4j.MongoDriver.*;
+import static neo4j.prometheusDriver.DealPrometheusRequest;
+>>>>>>> Stashed changes
+
+public class MongoDBController {
+    private MongoService mongoService;
+    @RequestMapping(value = "/api/storeEnvironment",method = RequestMethod.POST,produces = "application/json")
+    public Boolean storeAll(@RequestParam("masterName") String masterName, @RequestParam("podName") String podName, @RequestParam("serviceName") String serviceName, @RequestParam("address") String address, @RequestParam("namespace") String namespace){
+        return mongoService.storeAllService(masterName,podName,serviceName,address,namespace);
+    }
+    @RequestMapping(value = "/api/getAllByTime",method = RequestMethod.GET,produces = "application/json")
+    public Map<String, Object> getAllNLByTime(@RequestParam String time){
+        return getAllByTime(time);
+    }
+
+    @RequestMapping(value = "/api/getEventByTime", method = RequestMethod.POST, produces = "application/json")
+    public List<List> getEventByTime(@RequestParam("startDate")String startDate, @RequestParam("endDate")String endDate,@RequestParam("startTime")String startTime, @RequestParam("endTime")String endTime){
+        return getEventMongByTime(startDate,startTime,endDate, endTime);
+    }
+
+    private TimerUtil timerUtil = new TimerUtil();
+
+    @RequestMapping(value = "/api/openTimer", method = RequestMethod.POST, produces = "application/json")
+    public Boolean openTimer(@RequestBody Integer period){
+        try {
+            // 匿名方法
+            Runnable runnable = () -> {
+                // 把当前数据库中所有数据存到mongodb中
+                Map<String, Object> result = getNodesAndLinks();
+                save2Mongo(result);
+            };
+            final long time = 5;//延迟执行实际：5秒
+            timerUtil.scheduleAtFixedRate(runnable,time,period);
+            System.out.println("定时存储Mongo服务已开启！模拟数据变化频率："+period+"秒");
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("调用timer失败");
+            return false;
+        }
+        return true;
+    }
+
+    @RequestMapping(value = "/api/shutdownTimer", method = RequestMethod.POST, produces = "application/json")
+    public Boolean shutdownTimer(){
+        try {
+            timerUtil.shutdown();
+            System.out.println("定时存储Mongo服务已关闭");
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("调用timer失败");
+            return false;
+        }
+        return true;
+    }
+
+    @RequestMapping(value = "/api/StartCausation", method = RequestMethod.POST, produces = "application/json")
+    public Boolean StartCausation(HttpServletRequest request,@RequestParam("Start") String start,@RequestParam("End") String end){
+        String savePath = request.getSession().getServletContext().getRealPath("/WEB-INF/upload/csv");
+        savePath = savePath.replace("file:", "");
+        ArrayList arrayList = getAllQuery();
+        DealPrometheusRequest(start,end,arrayList,savePath);
+        return true;
+    }
+
+    @RequestMapping(value = "/api/addKapacitorEvent", method = RequestMethod.POST, produces = "application/json")
+    public Boolean addKapacitorEvent(@RequestBody String message){
+        //return readKapacitor(data);
+        System.out.println(message);
+        return saveKapacitor2Mongo(message);
+    }
+
+    //以下为Fuseki提供支持部分
+    @RequestMapping(value = "/api/getNodesAndLinks",method = RequestMethod.GET,produces = "application/json")
+    public Map<String, Object> getNodesAndLinks(){
+        return getAllNodesAndLinks();
+    }
+}
