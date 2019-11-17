962b9b28808fb747b1a2d3b6d9dbabe5d79d6a4a
sushaoci <sushaociqiqi@gmail.com>
2019-11-17 17:16:16 +0800

    修改了时间格式和页面样式,css不太会调

--- frontend19v/src/views/showData/Show.vue
+++ frontend19v/src/views/showData/Show.vue
@@ -1,25 +1,22 @@
 <template>
-  <div>
-    <chart :options="options" :init-options="initOptions" autoresize />
-    <div class="addCsv">
-      <input type="file" ref="csv" />
-      <el-button @click.prevent="onclick" icon="el-icon-search" circle></el-button>
-    </div>
-  </div>
-  <!-- <div class="Show">
+  <div class="Show">
     <div class="graph">
       <el-card class="box-card">
         <div slot="header" class="clearfix">
           <span>知识图谱</span>
         </div>
-        <div class="systemOverview"></div>
+        <div class="systemOverview">
+          <system/>
+        </div>
       </el-card>
 
       <el-card class="box-card2">
         <div slot="header" class="clearfix">
           <span>说明</span>
         </div>
-        <div class="text item"></div>
+        <div>
+          <chart :options="options" :init-options="initOptions" autoresize />
+        </div>
       </el-card>
     </div>
     <div class="word">
@@ -29,7 +26,6 @@
         </div>
         <div class="text item">
           <span>
-            <chart :options="options" :init-options="initOptions" autoresize />
           </span>
         </div>
       </el-card>
@@ -38,7 +34,7 @@
       <input type="file" ref="csv" />
       <el-button @click.prevent="onclick" icon="el-icon-search" circle></el-button>
     </div>
-  </div>-->
+  </div>
 </template>
 
 <script>
@@ -46,9 +42,12 @@ import Papa from "papaparse";
 import ECharts from "../../components/ECharts";
 import "echarts/lib/chart/line";
 
+import SystemOverview from '../KnowledgeGraph/SystemOverview'
+
 export default {
   components: {
-    chart: ECharts
+    chart: ECharts,
+    system:SystemOverview,
   },
   data() {
     return {
@@ -57,7 +56,7 @@ export default {
       },
       options: {
         xAxis: {
-          type: "time",
+          type: "category",
           data: []
         },
         yAxis: {
@@ -80,14 +79,32 @@ export default {
         reader.onload = env => {
           let result = env.target.result;
           let parsed = Papa.parse(result);
-          // console.log(parsed.data)
-          // console.log(parsed);
-          this.options.xAxis.data.push(...parsed.data[1]);
+          parsed.data[1].map(i => {
+            this.options.xAxis.data.push(this.timestampToTime(i));
+          });
           this.options.series.data.push(...parsed.data[0]);
-          console.log(JSON.stringify(this.options)) 
+          // console.log(JSON.stringify(this.options));
         };
         reader.onerror = function() {};
       }
+    },
+    timestampToTime(timestamp) {
+      var date = new Date(timestamp * 1000); //时间戳为10位需*1000，时间戳为13位的话不需乘1000
+      var Y = date.getFullYear() + "-";
+      var M =
+        (date.getMonth() + 1 < 10
+          ? "0" + (date.getMonth() + 1)
+          : date.getMonth() + 1) + "-";
+      var D =
+        (date.getDate() < 10 ? "0" + date.getDate() : date.getDate()) + " ";
+      var h =
+        (date.getHours() < 10 ? "0" + date.getHours() : date.getHours()) + ":";
+      var m =
+        (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()) +
+        ":";
+      var s =
+        date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+      return Y + M + D + h + m + s;
     }
   }
 };
@@ -99,7 +116,7 @@ export default {
   display: flex;
   flex-direction: row;
   width: 100%;
-  height: 300px;
+  height: 450px;
 }
 .text {
   font-size: 14px;
